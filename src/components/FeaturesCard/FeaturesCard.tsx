2fee275f9b00e9da743e350a3d22ac624044febb
Evgeny Kankin <evgenykankin@gmail.com>
2026-05-18 23:12:29 +0300

    in progress

--- src/components/FeaturesCard/FeaturesCard.tsx
+++ src/components/FeaturesCard/FeaturesCard.tsx
@@ -1,14 +1,14 @@
-import { IconGasStation, IconGauge, IconManualGearbox, IconUsers } from '@tabler/icons-react';
-import { Badge, Button, Card, Center, Group, Image, Text } from '@mantine/core';
+//import { IconGasStation, IconGauge, IconManualGearbox, IconUsers } from '@tabler/icons-react';
+//import { Badge, Button, Card, Center, Group, Image, Text } from '@mantine/core';
 import classes from './FeaturesCard.module.css';
-
+/*
 const mockdata = [
   { label: '4 passengers', icon: IconUsers },
   { label: '100 km/h in 4 seconds', icon: IconGauge },
   { label: 'Automatic gearbox', icon: IconManualGearbox },
   { label: 'Electric', icon: IconGasStation },
 ];
-
+*/
 type ProductCardProps = {
   name: string;
   price: number;
@@ -16,14 +16,15 @@ type ProductCardProps = {
 };
 
 export function FeaturesCard({image, name, price}:ProductCardProps) {
-  const features = mockdata.map((feature) => (
+  /*const features = mockdata.map((feature) => (
     <Center key={feature.label}>
       <feature.icon size={16} className={classes.icon} stroke={1.5} />
       <Text size="xs">{feature.label}</Text>
     </Center>
-  ));
+  ));*/
 
   return (
+    /*
     <Card withBorder radius="md" className={classes.card}>
       <Card.Section className={classes.imageSection}>
         <Image src = {image} alt="Tesla Model S" />
@@ -65,6 +66,23 @@ export function FeaturesCard({image, name, price}:ProductCardProps) {
           </Button>
         </Group>
       </Card.Section>
-    </Card>
+    </Card>*/
+
+    <div className={classes.productCard}>
+      <img className={classes.productImg} src={image} />
+      
+      <div className={classes.cardTitle}>
+        {name}
+        <button className ={classes.addCounterButton}></button>
+        <div className={classes.counter}><p>1</p></div>
+        <button className ={classes.removeCounterButton}></button>
+      </div>
+      
+      <div className={classes.priceSection}>
+        {price}
+        <button className={classes.addToCartButton}></button>
+      </div>
+    </div>
+
   );
 }
