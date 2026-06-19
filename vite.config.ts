12b4b88ada01a98ba6a7e3b76e3b210333bcce51
Evgeny Kankin <evgenykankin@gmail.com>
2026-06-19 17:33:00 +0300

    base fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2.3.2/',
+  base: '/4.2.2/',
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
